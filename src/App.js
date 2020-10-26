93a68b9ac3015d81f5d8b101728af7f3d9fe728e
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-10-26 16:18:52 -0600

    admin: integrate elastic UI, change everything to scss, dep lockfile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import React, { useState } from 'react';
+import { EuiCollapsibleNav, EuiButton, EuiTitle, EuiSpacer, EuiText, EuiCode } from '@elastic/eui';
+import './App.scss';
 
 function App() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+  const [navIsDocked, setNavIsDocked] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        cloudkit
-      </header>
-    </div>
+    <>
+      <EuiCollapsibleNav
+        isOpen={navIsOpen}
+        isDocked={navIsDocked}
+        button={
+          <EuiButton onClick={() => setNavIsOpen(!navIsOpen)}>
+            Toggle nav
+          </EuiButton>
+        }
+        onClose={() => setNavIsOpen(false)}>
+        <div style={{ padding: 16 }}>
+          <EuiTitle>
+            <h2>I am some nav</h2>
+          </EuiTitle>
+          <EuiSpacer />
+          <EuiButton
+            onClick={() => {
+              setNavIsDocked(!navIsDocked);
+            }}>
+            Docked: {navIsDocked ? 'on' : 'off'}
+          </EuiButton>
+        </div>
+      </EuiCollapsibleNav>
+
+      {navIsDocked && (
+        <EuiText size="s" color="subdued">
+          <p>
+            The button gets hidden by default when the nav is docked unless you
+            set <EuiCode language="js">showButtonIfDocked = true</EuiCode>.
+          </p>
+        </EuiText>
+      )}
+    </>
   );
 }
 
